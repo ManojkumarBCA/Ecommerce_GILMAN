a53f97bbc82dc4de6a52ff03b8f2949a77a191b0
manojkumarbca <manojcpf55@gmail.com>
2023-06-11 11:30:33 +0530

    Lock File Update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import path from "path";
 //configure env
 dotenv.config();
 
@@ -20,6 +20,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -27,9 +29,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome TO E-MART</h1>");
-});
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));  
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
